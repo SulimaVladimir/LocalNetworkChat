5743c94dc563e7f3694d241bfa552961b679b0cb
Vladimir Sulima <vladimirsulimawork@gmail.com>
2021-03-25 00:01:34 +0200

    Update ChatServer.java

--- server/src/ru/chat/server/ChatServer.java
+++ server/src/ru/chat/server/ChatServer.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.util.ArrayList;
 
+
 public class ChatServer implements TCPConnectionListener {
 
     public static void main(String[] args) {
